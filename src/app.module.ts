403ec153605c2fe8406a70d6b268079d963fdda0
ProVitSer <prokinvs@gmail.com>
2021-12-21 12:58:40 +0000

    add event

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { AmocrmModule } from './amocrm/amocrm.module';
 import { ConfigModule } from '@nestjs/config';
 import { LoggerModule } from './logger/logger.module';
+import { AsteriskModule } from './asterisk/asterisk.module';
 import configuration from './config/config.provider';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
-  imports: [ConfigModule.forRoot({ load: [configuration] }),AmocrmModule, LoggerModule],
+  imports: [ConfigModule.forRoot({ load: [configuration] }), EventEmitterModule.forRoot(),AmocrmModule, LoggerModule, AsteriskModule],
   controllers: [AppController],
   providers: [AppService],
   exports: [ConfigModule]
