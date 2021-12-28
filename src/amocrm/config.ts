8e0d8ce6f3b3d753c6e6bf968c278f588578f093
ProVitSer <prokinvs@gmail.com>
2021-12-28 07:25:07 +0000

    number map

---
+++ src/amocrm/config.ts
@@ -0,0 +1,35 @@
+import { operatorCIDNumber, OriginalCIDNumber } from '../config/config';
+
+export const sipTrunkMap: { [code in operatorCIDNumber]: OriginalCIDNumber } = {
+    [operatorCIDNumber.MOBILE1]: OriginalCIDNumber.MOBILE1,
+    [operatorCIDNumber.MOBILE2]: OriginalCIDNumber.MOBILE2,
+    [operatorCIDNumber.MOBILE3]: OriginalCIDNumber.MOBILE3,
+    [operatorCIDNumber.MOBILE4]: OriginalCIDNumber.MOBILE4,
+    [operatorCIDNumber.MOBILE5]: OriginalCIDNumber.MOBILE5,
+    [operatorCIDNumber.MOBILE6]: OriginalCIDNumber.MOBILE6,
+    [operatorCIDNumber.MOBILE7]: OriginalCIDNumber.MOBILE7,
+    [operatorCIDNumber.GOROD1]: OriginalCIDNumber.GOROD1,
+    [operatorCIDNumber.GOROD2]: OriginalCIDNumber.GOROD2,
+    [operatorCIDNumber.GOROD3]: OriginalCIDNumber.GOROD3,
+    [operatorCIDNumber.GOROD4]: OriginalCIDNumber.GOROD4,
+    [operatorCIDNumber.GOROD5]: OriginalCIDNumber.GOROD5,
+    [operatorCIDNumber.GOROD6]: OriginalCIDNumber.GOROD6,
+};
+
+
+export const numberDescriptionkMap: { [code in operatorCIDNumber]: string } = {
+    [operatorCIDNumber.MOBILE1]: "",
+    [operatorCIDNumber.MOBILE2]: "",
+    [operatorCIDNumber.MOBILE3]: "",
+    [operatorCIDNumber.MOBILE4]: "",
+    [operatorCIDNumber.MOBILE5]: "",
+    [operatorCIDNumber.MOBILE6]: "",
+    [operatorCIDNumber.MOBILE7]: "",
+    [operatorCIDNumber.GOROD1]: "",
+    [operatorCIDNumber.GOROD2]: "",
+    [operatorCIDNumber.GOROD3]: "SYN_61 Усадьба в Подмосковье",
+    [operatorCIDNumber.GOROD4]: "SYN_35 Усадьба в Подмосковье",
+    [operatorCIDNumber.GOROD5]: "SYN_39 Усадьба в Подмосковье",
+    [operatorCIDNumber.GOROD6]: "SYN_34 Усадьба в Подмосковье",
+};
+
