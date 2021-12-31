933c898cf3caed4b02f2c00de0c0b80ea68c36db
ProVitSer <prokinvs@gmail.com>
2021-12-31 16:38:01 +0300

    add end start sicket server

---
+++ src/socket/socket.module.ts
@@ -0,0 +1,12 @@
+import { AsteriskModule } from '@app/asterisk/asterisk.module';
+import { AxiosModule } from '@app/axios/axios.module';
+import { DatabaseModule } from '@app/database/database.module';
+import { LoggerModule } from '@app/logger/logger.module';
+import { Module } from '@nestjs/common';
+import { SocketGateway } from './socket.gateway';
+
+@Module({
+    imports: [LoggerModule, AsteriskModule, DatabaseModule, AxiosModule],
+    providers: [SocketGateway]
+})
+export class SocketModule {}
