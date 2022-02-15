74d46da159a75ae7d14aa7cee3f8e3ec2af20874
ProVitSer <prokinvs@gmail.com>
2022-02-15 13:13:40 +0000

    ref

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import configuration from '@app/config/config.provider';
 import httpsConfig from './https.provider';
+import { WsAdapter } from '@nestjs/platform-ws';
 
 
 async function bootstrap() {
   const config = new ConfigService(configuration());
   const httpsOptions = httpsConfig(config);
   const app = await NestFactory.create(AppModule, {...(httpsOptions ? { httpsOptions } : {})});
+  app.useWebSocketAdapter(new WsAdapter(app));
   //app.setGlobalPrefix('v1')
   await app.listen(config.get('appPort'));
 }
