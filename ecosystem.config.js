40e8025fcc9454f3814d309ffbdc69199612b6a6
ProVitSer <prokinvs@gmail.com>
2022-07-22 11:11:41 +0300

    add cluster/ env init

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,5 +5,11 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: false,
+      env_production: {
+        NODE_ENV: "production"
+     },
+     env_development: {
+        NODE_ENV: "development"
+     }
     }],
   }
