ccf9f26fc8deed59feb33a95e99051511ffc570e
ProVitSer <prokinvs@gmail.com>
2022-07-27 10:31:56 +0300

    add: endpoint for ari call

---
+++ src/asterisk/config.ts
@@ -0,0 +1,8 @@
+export const ARIOUTBOUNDCALLOPERATOR = 'Monitoring';
+
+export const ARIOUTBOUNDCALL = {
+    context: 'channel-dump',
+    priority: 1,
+    extension: '2222',
+    appArgs: 'dialed',
+}
